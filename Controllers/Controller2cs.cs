09ea74d80d32bf0857ec8db8779bedfdf6c491af
Minda Iulia Alexandra <iuliaminda@yahoo.com>
2022-10-31 21:22:44 +0200

    Added Lab1

---
+++ Controllers/Controller2cs.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Minda_Iulia_Lab1.Controllers
+{
+    public class Controller2cs : Controller
+    {
+        public string Index()
+        {
+            return "Welcome!";
+        }
+
+        public string Salut()
+        { return "Hello!"; }
+
+        public string Mesaj()
+        { return "Primul laborator"; }
+
+        public string Concatenare(string text, int numar)
+        {
+            string mesaj = text;
+            mesaj += " ";
+            mesaj += numar.ToString();
+            return mesaj;
+        }
+    }
+}
